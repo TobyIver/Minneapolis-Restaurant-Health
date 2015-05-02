8b35c86edb5519a90a818c5d71c06c2ee3d3a611
Toby Iverson <Toby558@gmail.com>
2015-05-02 17:58:55 -0500

    Everything but the Search!

--- app/src/main/java/com/iverson/toby/rhealth/MainActivity.java
+++ app/src/main/java/com/iverson/toby/rhealth/MainActivity.java
@@ -9,20 +9,6 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.database.Cursor;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -64,21 +50,10 @@ import org.xml.sax.InputSource;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.security.KeyStore;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -262,36 +237,7 @@ public class MainActivity extends Activity {
                     }
                 }
 
-               PlaceAdapter placeAdapter = new PlaceAdapter(MainActivity.this, R.layout.activity_main, places);
-                listView = (ListView)findViewById(R.id.httptestlist_listview);
-
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int pos,
-                                            long id) {
-                         CurrentPlace.place = places.get(pos);
-                        setContentView(R.layout.item_selected);
-
-                        // reformating name for use in CurrentViolations.v
-                        String pname = CurrentPlace.place.getName();
-                        pname = pname.toUpperCase();
-
-                        // running name violation query
-                        queryHealth = new StringBuilder();
-                        queryHealth.append("http://communities.socrata.com/resource/nzdy-gqv2.json");
-                        queryHealth.append("?$where=starts_with(name_of_business,'");
-                        queryHealth.append(pname + "')");
-
-                        String qHealth = queryHealth.toString();
-                        qHealth = qHealth.replaceAll(" ", "%20");
-                        //todo more validation
-                        new HealthCompare().execute(qHealth);
-
-                    }
-                });
-
-
-               listView.setAdapter(placeAdapter);
+            FilLStoreList();
 
             } catch (Exception e) {
                 Log.e("ERROR", e.getMessage());
@@ -301,7 +247,123 @@ public class MainActivity extends Activity {
         }
     }
 
+    public void FillPlace(){
 
+
+        setContentView(R.layout.item_selected);
+
+        TextView itemName = (TextView) findViewById(R.id.item_name);
+        TextView itemVicinity = (TextView) findViewById(R.id.item_vicinity);
+        TextView itemVRating = (TextView) findViewById(R.id.item_vrating);
+        TextView itemRating = (TextView) findViewById(R.id.item_rating);
+        TextView itemNegative = (TextView) findViewById(R.id.item_negative);
+
+        itemRating.setText(Float.toString(CurrentPlace.place.getRating()) + "/5 Google Rating");
+        itemName.setText(CurrentPlace.place.getName());
+        itemVicinity.setText(CurrentPlace.place.getVicinity());
+        itemVRating.setText(Integer.toString(CurrentPlace.place.getVRating()) + "/100 Health Score");
+        if(CurrentPlace.place.getVRating() < 0){
+            itemNegative.setText("Yes negative numbers are bad");
+        }
+
+
+
+
+
+        Button clickButton = (Button) findViewById(R.id.violations_button);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                setContentView(R.layout.violation_layout);
+
+
+
+                ViolationAdapter violationAdapter = new ViolationAdapter(MainActivity.this, R.layout.activity_main, CurrentViolations.all());
+                violationView = (ListView)findViewById(R.id.violation_listview);
+                violationView.setAdapter(violationAdapter);
+
+                Button returnButton = (Button) findViewById(R.id.back_button);
+                returnButton.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+
+
+                        FillPlace();
+
+                    }
+                });
+
+
+            };
+        });
+        Button mapButton = (Button) findViewById(R.id.map_button);
+        mapButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                float[] geometry =  CurrentPlace.place.getGeometry();
+
+                String uri = String.format(Locale.ENGLISH, "geo:"+ Float.toString(geometry[0]) + "," + Float.toString(geometry[1]));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(intent);
+
+                Toast.makeText(getApplicationContext(), "loading map " + uri,
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+        Button returnButton = (Button) findViewById(R.id.list_button);
+        returnButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                setContentView(R.layout.httptestlist);
+                FilLStoreList();
+            }
+        });
+
+    }
+
+
+
+
+    public void FilLStoreList() {
+
+
+    PlaceAdapter placeAdapter = new PlaceAdapter(MainActivity.this, R.layout.activity_main, places);
+    listView = (ListView)findViewById(R.id.httptestlist_listview);
+
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int pos,
+                                long id) {
+            CurrentPlace.place = places.get(pos);
+
+
+            // reformating name for use in CurrentViolations.v
+            String pname = CurrentPlace.place.getName();
+
+
+
+            new HealthAPI().execute(pname);
+
+
+
+        }
+    });
+
+
+    listView.setAdapter(placeAdapter);
+
+}
 
 
 
@@ -343,157 +405,8 @@ public class MainActivity extends Activity {
     }
 
 
-    // Pulls health inspection data from API
-    public class HealthCompare extends AsyncTask<String, String, String> {
-
-        @Override
-        protected String doInBackground(String... args) {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpResponse hresponse;
-            String responseString = null;
-            try {
-                hresponse = httpclient.execute(new HttpGet(args[0]));
-                StatusLine statusLine = hresponse.getStatusLine();
-                if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    hresponse.getEntity().writeTo(out);
-                    out.close();
-                    responseString = out.toString();
-                } else {
-                    //Closes the connection.
-                    hresponse.getEntity().getContent().close();
-                    throw new IOException(statusLine.getReasonPhrase());
-                }
-            } catch (ClientProtocolException e) {
-                Log.e("ERROR", e.getMessage());
-            } catch (IOException e) {
-                Log.e("ERROR", e.getMessage());
-            }
-            return responseString;
 
 
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            CurrentViolations.start();
-            JSONObject jsonObject = null;
-            try {
-                //pulling information out of Health JSON
-                JSONArray jarray = new JSONArray(result);
-                CurrentPlace.place.setVRating(101);
-                for (int i = 0; i < jarray.length(); i++) {
-                    JSONObject j = jarray.getJSONObject(i);
-
-                    String date = j.getString("date_of_inspection");
-                    String critical = j.getString("critical");
-                    String vname = j.getString("name_of_business");
-                    String codeViolation = j.getString("code_section");
-                    String riskLevel = j.getString("risk_level");
-                    String address = j.getString("license_address");
-                    String violationText = j.getString("standard_order_text");
-
-                    //verify violation results to prevent errors
-                    String paddress = CurrentPlace.place.getVicinity();
-                    paddress = paddress.toUpperCase();
-                    paddress = paddress.substring(0, paddress.indexOf(" "));
-                    String vaddress = address.substring(0, address.indexOf(" "));
-
-                    if (vaddress.equals(paddress)) {
-                        Violation violation = new Violation();
-                        violation.setName(vname);
-                        violation.setAddress(address);
-                        violation.setDate(date);
-                        violation.setRiskLevel(riskLevel);
-                        violation.setViolationText(violationText);
-                        violation.setCodeViolation(codeViolation);
-                        violation.setCritial(critical);
-
-
-                        // adding risk level to rating
-                        int r = 0;
-                        r = 2 * (4 - Integer.parseInt(riskLevel));  //risk levels 1-3, 1 being worse
-                        if (critical.equals("Yes")) {
-                            r = r * 3;
-                        }
-                        violation.setRating(r);
-                        CurrentPlace.place.setVRating(CurrentPlace.place.getVRating() - r);
-
-                        CurrentViolations.add(violation);
-                    }
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            TextView itemName = (TextView) findViewById(R.id.item_name);
-            TextView itemVicinity = (TextView) findViewById(R.id.item_vicinity);
-            TextView itemVRating = (TextView) findViewById(R.id.item_vrating);
-            TextView itemRating = (TextView) findViewById(R.id.item_rating);
-            TextView itemNegative = (TextView) findViewById(R.id.item_negative);
-
-            itemRating.setText(Float.toString(CurrentPlace.place.getRating()) + "/5 Google Rating");
-            itemName.setText(CurrentPlace.place.getName());
-            itemVicinity.setText(CurrentPlace.place.getVicinity());
-            itemVRating.setText(Integer.toString(CurrentPlace.place.getVRating()) + "/100 Health Score");
-            if(CurrentPlace.place.getVRating() < 0){
-                itemNegative.setText("Yes negative numbers are bad");
-            }
-            Button clickButton = (Button) findViewById(R.id.violations_button);
-            clickButton.setOnClickListener( new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-
-                    setContentView(R.layout.violation_layout);
-
-
-
-                    ViolationAdapter violationAdapter = new ViolationAdapter(MainActivity.this, R.layout.activity_main, CurrentViolations.all());
-                    violationView = (ListView)findViewById(R.id.violation_listview);
-                    violationView.setAdapter(violationAdapter);
-
-                    Button returnButton = (Button) findViewById(R.id.back_button);
-                    returnButton.setOnClickListener(new View.OnClickListener() {
-
-                        @Override
-                        public void onClick(View v) {
-                            setContentView(R.layout.item_selected);
-
-                        }
-                    });
-
-
-                };
-            });
-            Button mapButton = (Button) findViewById(R.id.map_button);
-            mapButton.setOnClickListener( new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-
-
-                    float[] geometry =  CurrentPlace.place.getGeometry();
-
-                    String uri = String.format(Locale.ENGLISH, "geo:"+ geometry[0] + "," + geometry[1]);
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                    startActivity(intent);
-
-                    Toast.makeText(getApplicationContext(), "loading map " + uri,
-                            Toast.LENGTH_LONG).show();
-
-                }
-            });
-        }
-    }
-
-    private class ItemSelected {
-
-
-
-    }
 
 
 
@@ -544,7 +457,111 @@ public class MainActivity extends Activity {
 
     }
 
+    public class HealthAPI extends AsyncTask<String, String, String> {
+        private StringBuilder queryHealth = new StringBuilder();
 
+        @Override
+        protected String doInBackground(String... args) {
+
+            //getting rid of special characters
+            String pname = args[0].toUpperCase();
+            pname = pname.replaceAll("&", "%26");
+            pname = pname.replaceAll("'", "%27");
+            pname = pname.replaceAll("!", "%21");
+            // running name violation query
+            queryHealth = new StringBuilder();
+            queryHealth.append("http://communities.socrata.com/resource/nzdy-gqv2.json");
+            queryHealth.append("?$where=starts_with(name_of_business,'");
+            queryHealth.append(pname + "')");
+
+            String qHealth = queryHealth.toString();
+            qHealth = qHealth.replaceAll(" ", "%20");
+
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse hresponse;
+            String responseString = null;
+            try {
+                hresponse = httpclient.execute(new HttpGet(qHealth));
+                StatusLine statusLine = hresponse.getStatusLine();
+                if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    hresponse.getEntity().writeTo(out);
+                    out.close();
+                    responseString = out.toString();
+                } else {
+                    //Closes the connection.
+                    hresponse.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                Log.e("ERROR", e.getMessage());
+            } catch (IOException e) {
+                Log.e("ERROR", e.getMessage());
+            }
+            return responseString;
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            CurrentViolations.start();
+            JSONObject jsonObject = null;
+            try {
+                //pulling information out of Health JSON
+                JSONArray jarray = new JSONArray(result);
+                CurrentPlace.place.setVRating(100);
+
+                for (int i = 0; i < jarray.length(); i++) {
+                    JSONObject j = jarray.getJSONObject(i);
+
+                    String date = j.getString("date_of_inspection");
+                    String critical = j.getString("critical");
+                    String vname = j.getString("name_of_business");
+                    String codeViolation = j.getString("code_section");
+                    String riskLevel = j.getString("risk_level");
+                    String address = j.getString("license_address");
+                    String violationText = j.getString("standard_order_text");
+
+                    //verify violation results to prevent errors
+                    String paddress = CurrentPlace.place.getVicinity();
+                    paddress = paddress.toUpperCase();
+                    paddress = paddress.substring(0, paddress.indexOf(" "));
+                    String vaddress = address.substring(0, address.indexOf(" "));
+
+                    if (vaddress.equals(paddress)) {
+                        Violation violation = new Violation();
+                        violation.setName(vname);
+                        violation.setAddress(address);
+                        violation.setDate(date);
+                        violation.setRiskLevel(riskLevel);
+                        violation.setViolationText(violationText);
+                        violation.setCodeViolation(codeViolation);
+                        violation.setCritial(critical);
+
+
+                        // adding risk level to rating
+                        int r = 0;
+                        r =  (4 - Integer.parseInt(riskLevel));  //risk levels 1-3, 1 being worse
+                        if (critical.equals("Yes")) {
+                            r = r * 4;
+                        }
+                        violation.setRating(r);
+                        CurrentPlace.place.setVRating(CurrentPlace.place.getVRating() - r);
+
+                        CurrentViolations.add(violation);
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            FillPlace();
+        }
+    }
 
     private class MyLocationListener implements LocationListener {
 
@@ -575,4 +592,6 @@ public class MainActivity extends Activity {
             myLocation.getLocation(getApplicationContext(), locationResult);
         }
     }
+
+
 }
